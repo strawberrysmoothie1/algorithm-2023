aeb29ff55748f698287932624fa4e5c9791d398f
진우 장 <jynwng@gmail.com>
2023-03-20 13:40:21 +0900

    gg

--- bye world/bye world/bye world.c
+++ bye world/bye world/bye world.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
-int main(void)
+
+int euclid(int a, int b)
 {
-	printf("ello world");
+	int num = 0;
+	int  n = 0;
+	n = a / b;
+
+	num = a - b * n;
+
+	printf("최대공약수 %d", num);
+	return 0;
+}
+
+
+int main(void) {
+	int a, b;
+	printf("2개의 정수 입력 >>");
+	scanf_s("%d %d", &a, &b);
+
+	euclid(a, b);
+	return 0;
 }
